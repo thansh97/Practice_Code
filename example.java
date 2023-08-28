6c9628cb5ce96277fcd2c3b0f98eaf68dc28db39
thanshdls97 <thanshd@gmail.com>
2023-08-28 15:23:41 +0530

    javaexample

---
+++ example.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class example {
+    public static void main (String args[]){
+        try{
+            System.out.println("enter the number");
+            Scanner sc = new Scanner(System.in);
+            int n= sc.nextInt();
+            int v=0;
+            int d=n%v;
+            System.out.println("number is divisible");
+        }catch (Exception e){
+            System.out.println("fix this exception"+e);
+
+        }
+        System.out.println("end of code");
+    }
+}
